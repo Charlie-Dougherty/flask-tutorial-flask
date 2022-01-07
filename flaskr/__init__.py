e64e393bd092e7b693a07a18174582c6ee91ffd4
hifideo <8526955+hifideo@users.noreply.github.com>
2022-01-07 13:29:57 +0100

    db.py initial commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
 
     #ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makdir(app.instance_path)
     except OSError:
         pass
 
